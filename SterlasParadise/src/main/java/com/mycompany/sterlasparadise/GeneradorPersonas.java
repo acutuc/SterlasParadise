993fe01ff03d4a36209eb20e3bcbdeeff3fddd3d
Eduardo <eduardoorellanapomares@gmail.com>
2022-04-26 12:27:06 +0200

    Añade los string con los nombres y apellidos.

--- SterlasParadise/src/main/java/com/mycompany/sterlasparadise/GeneradorPersonas.java
+++ SterlasParadise/src/main/java/com/mycompany/sterlasparadise/GeneradorPersonas.java
@@ -1,5 +1,8 @@
 
 package com.mycompany.sterlasparadise;
+
+import java.util.Random;
+
 /*
  *
  * @author eduardo
@@ -7,4 +10,23 @@ package com.mycompany.sterlasparadise;
  */
 public class GeneradorPersonas {
     
+    //Creamos un objeto Random
+    private static final Random random = new Random();
+    
+    //String con los nombres
+    private static final String[] NOMBRES = {"Gonzalo", "Antonio", "Lola", "Aitana",
+                                                 "Claudia", "Olivia", "Carmen", "Bruno",
+                                                 "Marcos", "Alex", "David", "Diego",
+                                                 "Manuel", "Noa", "Carla", "Daniela",
+                                                 "Leo", "Sofia", "Hugo", "Andreu", "Mireya",
+                                                 "Isidoro", "Pedro", "Pablo", "María", "Nerea", 
+                                                 "Javier", "Lucía", "Carlos", "Luna"};
+    //String con los apellidos
+    private static final String[] APELLIDOS = {"Muñoz", "Moreno", "Díaz", "Ruíz",
+                                                   "Hernandez", "Martín", "Gómez", "García",
+                                                   "Rodríguez", "González", "Fernández","López",
+                                                   "Martínez", "Sánchez", "Pérez", "Torres", "Ramos",
+                                                   "Gil", "Serrano", "Molina", "Blanco", "Castro",
+                                                   "Rubio", "Medina", "Castillo", "Santos", "Cruz",
+                                                   "Prieto", "Gallego", "Vega"};
 }
