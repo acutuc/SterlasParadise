e56e2fec029a9909da88bc89d62078c084abcf5c
Eduardo <eduardoorellanapomares@gmail.com>
2022-04-26 12:48:32 +0200

    Añade nuevo Array con extensiones de email. Crea clase generar emails aleatorios .Corrige errores en comentarios.

--- SterlasParadise/src/main/java/com/mycompany/sterlasparadise/GeneradorPersonas.java
+++ SterlasParadise/src/main/java/com/mycompany/sterlasparadise/GeneradorPersonas.java
@@ -13,20 +13,35 @@ public class GeneradorPersonas {
     //Creamos un objeto Random
     private static final Random random = new Random();
     
-    //String con los nombres
+    //Array de strings con los nombres
     private static final String[] NOMBRES = {"Gonzalo", "Antonio", "Lola", "Aitana",
-                                                 "Claudia", "Olivia", "Carmen", "Bruno",
-                                                 "Marcos", "Alex", "David", "Diego",
-                                                 "Manuel", "Noa", "Carla", "Daniela",
-                                                 "Leo", "Sofia", "Hugo", "Andreu", "Mireya",
-                                                 "Isidoro", "Pedro", "Pablo", "María", "Nerea", 
-                                                 "Javier", "Lucía", "Carlos", "Luna"};
-    //String con los apellidos
+                                             "Claudia", "Olivia", "Carmen", "Bruno",
+                                             "Marcos", "Alex", "David", "Diego",
+                                             "Manuel", "Noa", "Carla", "Daniela",
+                                             "Leo", "Sofia", "Hugo", "Andreu", "Mireya",
+                                             "Isidoro", "Pedro", "Pablo", "María", "Nerea", 
+                                             "Javier", "Lucía", "Carlos", "Luna"};
+    
+    //Array de strings con los apellidos
     private static final String[] APELLIDOS = {"Muñoz", "Moreno", "Díaz", "Ruíz",
-                                                   "Hernandez", "Martín", "Gómez", "García",
-                                                   "Rodríguez", "González", "Fernández","López",
-                                                   "Martínez", "Sánchez", "Pérez", "Torres", "Ramos",
-                                                   "Gil", "Serrano", "Molina", "Blanco", "Castro",
-                                                   "Rubio", "Medina", "Castillo", "Santos", "Cruz",
-                                                   "Prieto", "Gallego", "Vega"};
+                                               "Hernandez", "Martín", "Gómez", "García",
+                                               "Rodríguez", "González", "Fernández","López",
+                                               "Martínez", "Sánchez", "Pérez", "Torres", "Ramos",
+                                               "Gil", "Serrano", "Molina", "Blanco", "Castro",
+                                               "Rubio", "Medina", "Castillo", "Santos", "Cruz",
+                                               "Prieto", "Gallego", "Vega"};
+    
+    //Array de strings con las extensiones de email posibles 
+    private static final String[] EXTENSIONES = {"@yahoo.es", "@hotmail.com", "@gmail.com",
+                                                 "@outlook.es", "@msn.com"};
+    
+    //Clase que genera emails con los nombres y apellidos de cada persona
+    public String GenerarEmail(String nombre, String apellido1, String apellido2){
+        String email = (nombre + apellido1 + apellido2 + EXTENSIONES[random.nextInt(EXTENSIONES.length)]);
+        return email;
+    }
+    
+    
+    
+    
 }
