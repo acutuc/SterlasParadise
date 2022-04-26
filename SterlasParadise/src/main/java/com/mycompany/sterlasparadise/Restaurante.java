0d8894b7085443bdae46d0e55969d569137282aa
Maxi <mhof0902@g.educaand.es>
2022-04-26 12:29:51 +0200

    Clase restaurante Comentada

--- SterlasParadise/src/main/java/com/mycompany/sterlasparadise/Restaurante.java
+++ SterlasParadise/src/main/java/com/mycompany/sterlasparadise/Restaurante.java
@@ -4,19 +4,23 @@ import java.util.ArrayList;
 
 public class Restaurante {
     
+    //Atributos de la clase:
     private String nombre;
     private String direccion;
     private ArrayList<Zona> zonas; 
 
+    //Metodo Constructor sin parametros
     public Restaurante() {
     }
 
+    //Metodo Constructor con parametros
     public Restaurante(String nombre, String direccion, ArrayList<Zona> zonas) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.zonas = zonas;
     }
 
+    //Getters y Setters de los a tributos de la clase:
     public String getNombre() {
         return nombre;
     }
@@ -40,7 +44,9 @@ public class Restaurante {
     public void setZonas(ArrayList<Zona> zonas) {
         this.zonas = zonas;
     }
+    //Fin de los Getters y Setters y de la clase
 
+    //Metodo ToString 
     @Override
     public String toString() {
         return "Restaurante{" + "nombre=" + nombre + ", direccion=" + direccion + ", zonas=" + zonas + '}';
