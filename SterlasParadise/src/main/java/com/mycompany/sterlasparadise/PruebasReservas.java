362a51c6a8d935a52adb879963ea63d998e0f33a
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-04-29 08:59:43 +0200

    Cambio para generar cada vez un número aleatrorio de reservas

--- SterlasParadise/src/main/java/com/mycompany/sterlasparadise/PruebasReservas.java
+++ SterlasParadise/src/main/java/com/mycompany/sterlasparadise/PruebasReservas.java
@@ -1,16 +1,21 @@
 package com.mycompany.sterlasparadise;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class PruebasReservas {
 
     public static void main(String[] args) {
         
+        //Generador de un número aleatorio para la generación de solicitudes de reservas.
+        Random rd = new Random();
+        int reserv = rd.nextInt(25)+1;
+        
         //Lista para guardas las reservas.
         ArrayList<ReservasAleatorias> reservas = new ArrayList<>();
         
         //Bucle for para generar el número de reservas que queramos, en este caso 20
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < reserv; i++) {
             reservas.add(new ReservasAleatorias());
         }
         //Bucle for para imprimir todas las reservas.
