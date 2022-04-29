5e384afa33c3195210228388a244ef62ff419a6e
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-04-29 09:54:27 +0200

    Cambios despreciables

--- SterlasParadise/src/main/java/com/mycompany/sterlasparadise/PruebasGenerales.java
+++ SterlasParadise/src/main/java/com/mycompany/sterlasparadise/PruebasGenerales.java
@@ -18,7 +18,7 @@ public class PruebasGenerales {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Persona p1 = new Persona("Mein", "Herz", "987654321", "meinherzbrent@outlook.de", LocalDate.of(1980, 02, 15));
+        Persona p1 = new Persona();
         
         System.out.println(p1);
     }
