aa295d10c3685ccd13b63bd9ad4e6aadba2b56d1
Xelemental <xelemental864@gmail.com>
2022-05-06 10:54:16 +0200

    Datos introducidos en mesas_disponibles. Funciona

---
+++ SQL/prueba_mesas_disponibles.sql
@@ -0,0 +1,11 @@
+use bdsterlasparadise;
+
+insert into mesas_disponibles
+values
+(1,1,"2003-6-24", "14:00", 14),
+(2,1,"2009-4-15", "14:30", 8),
+(1,2,"2015-11-5", "16:00", 2),
+(2,2,"2019-10-19", "15:00", 0),
+(1,3,"2020-1-31", "13:30", 5);
+
+select * from mesas_disponibles;
