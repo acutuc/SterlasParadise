7ef5f28aa1b79d75e2535ab84615a3b17b90ff4c
miguel <miguelmartinfernandez94@gmail.com>
2022-05-06 09:31:01 +0200

    se introucen datos en la tabla zonas

---
+++ SQL/introducirzonas.sql
@@ -0,0 +1,9 @@
+INSERT INTO zonas
+-- interio impar exterior par, se empieza por 1, planta baja 0, si no se espeficia 0
+VALUES
+		(1, 1, 1, 0, 10),
+        (2, 1, 1, 0, 10),
+        (2, 2 , 0, 0, 5),
+        (3, 1, 1, 0, 5),
+        (3, 3, 1, 1, 10),
+        (4, 1, 1, 0, 20);
