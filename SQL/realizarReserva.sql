a11905fc501db46b90380753daf26e62f75626d0
Sergio <sromero147@gmail.com>
2022-05-09 13:02:51 +0200

    Funcion para realizar reservas creada

---
+++ SQL/realizarReserva.sql
@@ -0,0 +1,20 @@
+delimiter $$
+drop function if exists realizarReserva $$
+create function realizarReserva
+(codReserva int,fechaReserva date,horaReserva time,numeroPersonas int,nombreCliente varchar(30),
+apellicdoCliente varchar(30),telefono char(9),emailCliente varchar(100),codZona int,codRestaurante int)
+returns boolean
+deterministic
+begin
+	if reservaPosible() then
+    begin
+		insert into reservas (codreserva,fecres,horares,numper,nomcli,apecli,tlfcli,emailcli,codzona,codrest)
+        values
+        (codReserva,fechaReserva,horaReserva,numeroPersonas,nombreCliente,apellicdoCliente,telefono,emailCliente,
+        codZona,codRestaurante);
+        return true;
+    end;
+    else 
+    return false;
+    end if;
+end $$
