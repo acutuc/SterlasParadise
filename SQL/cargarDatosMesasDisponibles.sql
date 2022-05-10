c1681208013f01eedd4ddf1f940f6a0c136e6fae
JaviAkaSino <javiakasino@hotmail.com>
2022-05-10 12:51:29 +0200

    cargarDatos funciona, probado hasta 15 días

--- SQL/cargarDatosMesasDisponibles.sql
+++ SQL/cargarDatosMesasDisponibles.sql
@@ -1,3 +1,4 @@
+use bdsterlasparadise;
 DROP PROCEDURE IF EXISTS cargarDatosMesasDisponibles;
 delimiter $$
 CREATE PROCEDURE cargarDatosMesasDisponibles()
@@ -7,27 +8,30 @@ BEGIN
 	DECLARE contadorTurnos int default 0;
 	DELETE FROM mesas_disponibles; -- Borra los datos existentes
 
-    WHILE contadorDias < 60 DO -- 60
+    WHILE contadorDias < 15 DO -- 60 días
 		BEGIN
 				-- Hawai
-                -- Turnos comida zona 1
+                SET contadorTurnos = 0;
+                -- Turnos comida zona 1                
                 WHILE contadorTurnos < 6 DO
 					BEGIN
 						INSERT INTO mesas_disponibles -- De 13 a 15:30 y 20 a 22:30:
 						VALUES
 							(1,1, date_add(curdate(),interval contadorDias DAY), 
-                            date_add('13:00:00', interval contadorTurnos*30 MINUTE) , 10);
+                            (time '13:00:00' + interval contadorTurnos*30 minute) , 10);
+						SET contadorTurnos = contadorTurnos + 1;
                     END;
 				END WHILE;
                 
                 SET contadorTurnos = 0;
                 -- Turnos comida zona 2
-                WHILE contadorTurno < 6 DO
+                WHILE contadorTurnos < 6 DO
 					BEGIN
 						INSERT INTO mesas_disponibles -- De 13 a 15:30 y 20 a 22:30:
 						VALUES
 							(1,2, date_add(curdate(),interval contadorDias DAY), 
-                            date_add('13:00:00', interval contadorTurnos*30 MINUTE) , 10);
+                            (time '13:00:00' + interval contadorTurnos*30 minute) , 10);
+						SET contadorTurnos = contadorTurnos + 1;
                     END;
 				END WHILE;
                 
@@ -39,25 +43,119 @@ BEGIN
 						INSERT INTO mesas_disponibles -- De 13 a 15:30 y 20 a 22:30:
 						VALUES
 							(2,1, date_add(curdate(),interval contadorDias DAY), 
-                            date_add('13:00:00', interval contadorTurnos*30 MINUTE) , 10);
+                            (time '13:00:00' + interval contadorTurnos*30 minute) , 10);
+						SET contadorTurnos = contadorTurnos + 1;
                     END;
 				END WHILE;
                 
                 
                 -- Turnos comida zona 2
                 SET contadorTurnos = 0;
-                WHILE contadorTurno < 6 DO
+                WHILE contadorTurnos < 6 DO
+					BEGIN
+						INSERT INTO mesas_disponibles -- De 13 a 15:30
+						VALUES
+							(2,2, date_add(curdate(),interval contadorDias DAY), 
+							(time '13:00:00' + interval contadorTurnos*30 minute) , 5);
+						SET contadorTurnos = contadorTurnos + 1;
+                    END;
+				END WHILE;
+                
+                -- Turnos cena zona 1
+                SET contadorTurnos = 0;
+                WHILE contadorTurnos < 6 DO
+					BEGIN
+						INSERT INTO mesas_disponibles -- De 20 a 22:30:
+						VALUES
+							(2,1, date_add(curdate(),interval contadorDias DAY), 
+                            (time '20:00:00' + interval contadorTurnos*30 minute) , 10);
+						SET contadorTurnos = contadorTurnos + 1;
+                    END;
+				END WHILE;
+                
+                
+                -- Turnos cena zona 2
+                SET contadorTurnos = 0;
+                WHILE contadorTurnos < 6 DO
+					BEGIN
+						INSERT INTO mesas_disponibles -- De 20 a 22:30:
+						VALUES
+							(2,2, date_add(curdate(),interval contadorDias DAY), 
+                            (time '20:00:00' + interval contadorTurnos*30 minute) , 5);
+						SET contadorTurnos = contadorTurnos + 1;
+                    END;
+				END WHILE;
+                
+                 -- Marbella
+                -- Turnos comida zona 1
+                SET contadorTurnos = 0;
+                WHILE contadorTurnos < 6 DO
 					BEGIN
 						INSERT INTO mesas_disponibles -- De 13 a 15:30 y 20 a 22:30:
 						VALUES
-							(2,2, date_add(curdate(),interval contadorDias DAY), 
-                            date_add('13:00:00', interval contadorTurnos*30 MINUTE) , 5);
+							(3,1, date_add(curdate(),interval contadorDias DAY), 
+                            (time '13:00:00' + interval contadorTurnos*30 minute) , 5);
+						SET contadorTurnos = contadorTurnos + 1;
                     END;
 				END WHILE;
-
+                
+                
+                -- Turnos comida zona 3
+                SET contadorTurnos = 0;
+                WHILE contadorTurnos < 6 DO
+					BEGIN
+						INSERT INTO mesas_disponibles -- De 13 a 15:30 y 20 a 22:30:
+						VALUES
+							(3,3, date_add(curdate(),interval contadorDias DAY), 
+                            (time '13:00:00' + interval contadorTurnos*30 minute) , 10);
+						SET contadorTurnos = contadorTurnos + 1;
+                    END;
+				END WHILE;
+                
+                -- Turnos cena zona 1
+                SET contadorTurnos = 0;
+                WHILE contadorTurnos < 6 DO
+					BEGIN
+						INSERT INTO mesas_disponibles -- De 20 a 22:30:
+						VALUES
+							(3,1, date_add(curdate(),interval contadorDias DAY), 
+                            (time '20:00:00' + interval contadorTurnos*30 minute) , 5);
+						SET contadorTurnos = contadorTurnos + 1;
+                    END;
+				END WHILE;
+                
+                
+                -- Turnos cena zona 3
+                SET contadorTurnos = 0;
+                WHILE contadorTurnos < 6 DO
+					BEGIN
+						INSERT INTO mesas_disponibles -- De 20 a 22:30:
+						VALUES
+							(3,3, date_add(curdate(),interval contadorDias DAY), 
+                            (time '20:00:00' + interval contadorTurnos*30 minute) , 10);
+						SET contadorTurnos = contadorTurnos + 1;
+                    END;
+				END WHILE;
+                
+                -- London
+                -- Turnos cena zona 1
+                SET contadorTurnos = 0;
+                WHILE contadorTurnos < 6 DO
+					BEGIN
+						INSERT INTO mesas_disponibles -- De 13 a 15:30 y 20 a 22:30:
+						VALUES
+							(4,1, date_add(curdate(),interval contadorDias DAY), 
+                            (time '20:00:00' + interval contadorTurnos*30 minute) , 20);
+						SET contadorTurnos = contadorTurnos + 1;
+                    END;
+				END WHILE;
+                
+                
+                SET contadorDias = contadorDias + 1;
+                
         END;
     END WHILE;
 END$$
 delimiter ;
 
-call cargarDatosMesasDisponibles();
+-- call cargarDatosMesasDisponibles();
