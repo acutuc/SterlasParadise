1e6795a9ed90a755bec070817a63c805e7753c52
Sergio <sromero147@gmail.com>
2022-05-10 10:00:14 +0200

    Funcion realizarReserva añadida al script general

--- SQL/bdsterlasparadise.sql
+++ SQL/bdsterlasparadise.sql
@@ -165,6 +165,32 @@ end $$
 
 
 -- Realizar reserva
+delimiter $$
+drop function if exists realizarReserva $$
+create function realizarReserva
+(codReserva int,fechaReserva date,horaReserva time,numeroPersonas int,nombreCliente varchar(30),
+apellicdoCliente varchar(30),telefono char(9),emailCliente varchar(100),codZona int,codRestaurante int)
+returns boolean
+deterministic
+begin
+	if reservaPosible() then
+    begin
+		insert into reservas (codreserva,fecres,horares,numper,nomcli,apecli,tlfcli,emailcli,codzona,codrest)
+        values
+        (codReserva,fechaReserva,horaReserva,numeroPersonas,nombreCliente,apellicdoCliente,telefono,emailCliente,
+        codZona,codRestaurante);
+        update mesas_disponibles
+        set numMesas=numMesas-mesasNecesarias(numeroPersonas)
+        where codRestaurante=codrest and
+								codzona=zona and
+								fecha=fechas and
+								hora=horas;
+        return true;
+    end;
+    else 
+    return false;
+    end if;
+end $$
 
 -- Datos mesas_disponibles (2 pts.)
  -- Rutina que rellena los 10 primeros días de la tabla mesas_disponibles
