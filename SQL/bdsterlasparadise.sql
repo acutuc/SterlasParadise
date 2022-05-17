219641b41a2a1cb221c3369aca0c5e63b3ebffa5
Sergio <sromero147@gmail.com>
2022-05-17 12:01:52 +0200

    Generacion automatica del codigo de reserva de manera automatica

--- SQL/bdsterlasparadise.sql
+++ SQL/bdsterlasparadise.sql
@@ -169,13 +169,17 @@ end $$
 delimiter $$
 drop function if exists realizarReserva $$
 create function realizarReserva
-(codReserva int,fechaReserva date,horaReserva time,numeroPersonas int,nombreCliente varchar(30),
+(fechaReserva date,horaReserva time,numeroPersonas int,nombreCliente varchar(30),
 apellicdoCliente varchar(30),telefono char(9),emailCliente varchar(100),zona int,restaurante int)
 returns boolean
 deterministic
 begin
 	if reservaPosible(restaurante, zona, numeroPersonas, fechaReserva, horaReserva) then
     begin
+		declare codReserva int;
+		select ifnull(max(codreserva)+1,1) into codReserva
+        from reservas;
+    
 		insert into reservas (codreserva,fecres,horares,numper,nomcli,apecli,tlfcli,emailcli,codzona,codrest)
         values
         (codReserva,fechaReserva,horaReserva,numeroPersonas,nombreCliente,apellicdoCliente,telefono,emailCliente,
